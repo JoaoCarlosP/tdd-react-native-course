2a89857f0b1ec780d27207291d03d1157d74a601
JoaoCarlosP <joaocarlosjoe74@gmail.com>
2023-09-28 09:31:34 -0300

    added tests and dependencies

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import App from './App'
+
+
+describe('test', () => {
+  test('Deve testar', () =>{
+    renderer.create(<App />)
+  })
+})
